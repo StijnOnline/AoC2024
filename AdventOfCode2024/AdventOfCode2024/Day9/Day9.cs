20c49ba903f598455a976591d1e14e5c0ed6a167
StijnOnline <stijnvandeijzen@hotmail.nl>
2024-12-09 21:02:55 +0100

    Day9 Star2

--- AdventOfCode2024/AdventOfCode2024/Day9/Day9.cs
+++ AdventOfCode2024/AdventOfCode2024/Day9/Day9.cs
@@ -10,9 +10,10 @@ internal class Day9 : Day
         }
         public int ID;
         public Range range;
+        public int length => range.length;
         public override string ToString()
         {
-            return $"{ID}: [{range.start}-{range.end}] l={range.length}";
+            return $"{ID}: [{range.start}-{range.end}] l={length}";
         }
     }
     public struct Range
@@ -41,11 +42,15 @@ internal class Day9 : Day
     {
         List<Data> data = new List<Data> (input.Length);
         int startIndex = 0;
+        var ID=0;
         for (int i = 0; i < input.Length; i++)
         {
             int value = (input[i] - '0');
-            var ID = i % 2 == 0 ? i / 2 : -1;
-            data.Add(new Data(ID, new Range(startIndex, startIndex + value-1)));
+            if(i % 2 == 0)
+            {
+                data.Add(new Data(ID, new Range(startIndex, startIndex + value - 1)));
+                ID++;
+            }
             startIndex += value;
         }
         return data;
@@ -54,43 +59,52 @@ internal class Day9 : Day
     public string Star1(string input, bool example = false)
     {
         var data = ParseInput(input);
-        var result = Defrag(data.ToList());
+        var result = Compress(data.ToList());
         if(example)
             Console.WriteLine(string.Join("", result.Select(d => new string( d.ID.ToString()[0],d.range.length) )));
         return result.Select(d=> (long) d.ID*d.range.SumPositions()).Sum().ToString();
     }
     public string Star2(string input, bool example = false)
     {
-        return "";
+        var data = ParseInput(input);
+        var result = Compress2(data.ToList());
+        if (example){
+            for (int i = 0; i < result.Count; i++)
+            {
+                Console.Write(new string(result[i].ID.ToString()[0], result[i].range.length));
+                if(i < result.Count-1)
+                    Console.Write(new string('.', result[i + 1].range.start - result[i].range.end-1));
+            }
+            Console.WriteLine();
+        }
+        return result.Select(d => (long)d.ID * d.range.SumPositions()).Sum().ToString();
+        //Guess1: 9863344581804 too high
     }
 
-    public List<Data> Defrag(List<Data> data)
+    public List<Data> Compress(List<Data> data)
     {
         var defragmentedData = new List<Data> (data.Count);
         while (data.Count > 0)
         {
             //Add data from left
             defragmentedData.Add(data[0]);
-            data.RemoveAt(0);
-
-            //get gap
-            if(data.Count == 0)
-                break;
-            var gap = data[0].range;
-            data.RemoveAt(0);
-
-            if (data.Count == 0){
+            if (data.Count <= 1)
+            {
                 break;
             }
+            var gap = new Range(data[0].range.end+1, data[1].range.start-1);
+            data.RemoveAt(0);
+
             var last = data.Last();
             while (data.Count > 0) {
+                if (last.Equals(default))
+                    break;
+
                 if (gap.length <= last.range.length)
                 {
                     defragmentedData.Add(new (last.ID, gap));
                     if (gap.length == last.range.length) {
-                        data.RemoveAt(data.Count - 1);
-                        if(data.Count>0) 
-                            data.RemoveAt(data.Count - 1);
+                        data.Remove(last);
                     }
                     else
                         data[data.Count - 1] = new Data(last.ID, new Range(last.range.start, last.range.end - gap.length));
@@ -101,14 +115,39 @@ internal class Day9 : Day
                 {
                     defragmentedData.Add(new(last.ID, new Range(gap.start, gap.start + last.range.length-1)));
                     gap = new Range(gap.start + last.range.length, gap.end);
-                    data.RemoveAt(data.Count - 1);
+                    data.Remove(last);
                     if (data.Count > 0)
-                        data.RemoveAt(data.Count - 1);
-                    last = data.Last();
+                        last = data.Last();
                 }
+            }
+            if (last.Equals(default))
+            {
+                break;
             }
         }
         return defragmentedData;
+    }
+
+    public List<Data> Compress2(List<Data> data)
+    {
+        for (int checkIndex = data.Count-1; checkIndex > 0; checkIndex--)
+        {
+            var lengthReq = data[checkIndex].length;
+
+            for (int j = 0; j < checkIndex; j++)
+            {
+                var gap = data[j + 1].range.start - data[j].range.end - 1;
+                if (gap < lengthReq)
+                    continue;
+
+                data.Insert(j + 1, new Data(data[checkIndex].ID, new Range(data[j].range.end + 1, data[j].range.end + lengthReq)));
+                data.RemoveAt(checkIndex+1);
+                checkIndex++;
+                break;
+            }
+
+        }
+        return data;
     }
 }
 
