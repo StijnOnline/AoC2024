7597f0c83b77ea30dacb24858a65521483dbfd34
StijnOnline <stijnvandeijzen@hotmail.nl>
2024-12-11 19:54:59 +0100

    Folder renaming

--- AdventOfCode2024/AdventOfCode2024/Input.cs
+++ AdventOfCode2024/AdventOfCode2024/Input.cs
@@ -1,12 +1,12 @@
 ﻿public static class Input
 {
-    const string basePath = @"..\..\..\Day"; //from build folder back to project folder
+    const string basePath = @"..\..\..\Day "; //from build folder back to project folder
     public static string ReadTestInput(int dayNumber)
     {
-        return File.ReadAllText(basePath + dayNumber + "/TestInput.txt");
+        return File.ReadAllText(basePath + dayNumber.ToString("00") + "/TestInput.txt");
     }
     public static string ReadInput(int dayNumber)
     {
-        return File.ReadAllText(basePath + dayNumber + "/Input.txt");
+        return File.ReadAllText(basePath + dayNumber.ToString("00") + "/Input.txt");
     }
 }
