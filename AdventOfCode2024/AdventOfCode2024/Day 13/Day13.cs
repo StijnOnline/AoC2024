3df8ae1381b88c9efc805897ac81ad2e36f5df60
StijnOnline <stijnvandeijzen@hotmail.nl>
2024-12-17 21:49:09 +0100

    Day13

--- AdventOfCode2024/AdventOfCode2024/Day 13/Day13.cs
+++ AdventOfCode2024/AdventOfCode2024/Day 13/Day13.cs
@@ -6,13 +6,13 @@ using System.Text.RegularExpressions;
 
 internal class Day13 : Day
 {
-    public List<int[]> ParseInput(string input)
+    public List<long[]> ParseInput(string input)
     {
         var matches = Regex.Matches(input, @"\d+");
-        var machines = new List<int[]>();
+        var machines = new List<long[]>();
         for (int i = 0; i < matches.Count; i += 6)
         {
-            machines.Add(matches.Skip(i).Take(6).Select(m => int.Parse(m.Value)).ToArray());
+            machines.Add(matches.Skip(i).Take(6).Select(m => long.Parse(m.Value)).ToArray());
         }
         return machines;
     }
@@ -21,12 +21,16 @@ internal class Day13 : Day
     {
         var machines = ParseInput( input);
         return machines.Sum(m=>TokensToWin(m)).ToString();
-
-        //Guess2 23395 too low
     }
     public string Star2(string input, bool example = false)
     {
-        return "";
+        var machines = ParseInput(input);
+        foreach (var machine in machines)
+        {
+            machine[4] += 10000000000000;
+            machine[5] += 10000000000000;
+        }
+        return machines.Sum(m => TokensToWin(m)).ToString();
     }
 
     /*
@@ -48,30 +52,43 @@ internal class Day13 : Day
     (for denominator != 0)
     */
 
-    public int? SolveA(decimal Xa, decimal Ya, decimal Xb, decimal Yb, decimal X, decimal Y)
+    public long? SolveA(decimal Xa, decimal Ya, decimal Xb, decimal Yb, decimal X, decimal Y)
     {
         var denominator = Ya - (Yb * Xa) / Xb;
-        if (denominator == 0) 
+        if (denominator==0) 
             return null;
         var numerator = Y - (Yb * X) / Xb;
         var A = (numerator / denominator);
-        return decimal.IsInteger(A) ? (int) A : null;
+        if (Math.Abs(A - Math.Round(A)) < 1e-10m)
+            return (long)(Math.Round(A));
+        else 
+            return null;
     }
-    public int SolveB(decimal A,decimal X, decimal Xa, decimal Xb)
+    public long SolveB(decimal A,decimal X, decimal Xa, decimal Xb)
     {
-        return (int)((X - Xa * A) / Xb);
+        return (long)Math.Round((X - Xa * A) / Xb);
     }
 
 
     //just so i can try spread operator
-    public int? SolveA_spread(params int[] l) => SolveA(l[0], l[1], l[2], l[3], l[4], l[5]);
-    public int SolveB_spread(int A,params int[] l) => SolveB(A,l[4], l[0], l[2]);
+    public long? SolveA_spread(params long[] l) => SolveA(l[0], l[1], l[2], l[3], l[4], l[5]);
+    public long SolveB_spread(long A,params long[] l) => SolveB(A,l[4], l[0], l[2]);
 
-    public int TokensToWin(int[] machineNumbers)
+    public long TokensToWin(long[] machineNumbers)
     {
         var A = SolveA_spread(machineNumbers);
-        if (!A.HasValue) return 0;
+        if (!A.HasValue) 
+            return 0;
         var B = SolveB_spread(A.Value, machineNumbers);
+
+        var X = machineNumbers[0] * A + machineNumbers[2] * B;
+        if (machineNumbers[4] != X)
+            return 0;
+
+        var Y = machineNumbers[1] * A + machineNumbers[3] * B;
+        if (machineNumbers[5] != Y)
+            return 0;
+
         return A.Value * 3 + B;
     }
 
